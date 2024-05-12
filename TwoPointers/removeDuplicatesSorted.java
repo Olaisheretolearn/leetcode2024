b8c61fdfe4e37bcb6a84363c3a25ec6211f03dbd
Olaoluwa Oke <olaoluwa324@gmail.com>
2024-05-11 23:55:57 -0500

    ...

--- TwoPointers/removeDuplicatesSorted.java
+++ TwoPointers/removeDuplicatesSorted.java
@@ -9,7 +9,7 @@ Custom Judge:
 
 The judge will test your solution with the following code:
 
-int[] nums = [...]; // Input array
+int[] nums = [...]; // Input arrayyy
 int[] expectedNums = [...]; // The expected answer with correct length
 
 int k = removeDuplicates(nums); // Calls your implementation
@@ -18,6 +18,7 @@ assert k == expectedNums.length;
 for (int i = 0; i < k; i++) {
     assert nums[i] == expectedNums[i];
 }
+***
  */
 
 
